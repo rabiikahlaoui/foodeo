29fa51a1873e14eda8eb582a265acc6cdb670af6
Rabii Kahlaoui <rabii.kahlaouiii@gmail.com>
2021-01-10 18:15:12 +0100

    Add Laravel Jetstream

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'index']);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
-
-Route::get('/home', [HomeController::class, 'index']);
